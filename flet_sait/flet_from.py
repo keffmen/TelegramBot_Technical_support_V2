2134905c4e795c76722628d6c65f4273165fa55b
keffmen <91837001+keffmen@users.noreply.github.com>
2024-02-05 16:20:26 +0700

    Сайт Версии 1

--- flet_sait/flet_from.py
+++ flet_sait/flet_from.py
@@ -1,8 +1,19 @@
 import flet as ft
 
 
-async def main(page: ft.Page):
-    await page.add_async(ft.Text("Hello, async world!"))
+def main(page):
+    def btn_click(e):
+        if not txt_name.value:
+            txt_name.error_text = "Please enter your name"
+            page.update()
+        else:
+            name = txt_name.value
+            page.clean()
+            page.add(ft.Text(f"Hello, {name}!"))
+
+    txt_name = ft.TextField(label="Your name")
+
+    page.add(txt_name, ft.ElevatedButton("Say hello!", on_click=btn_click))
 
 
 ft.app(target=main)
